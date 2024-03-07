b96b8ce980b54b4d15f22dbd29f5a0ce7e7c08a2
arfanazmol <arfanazmol66@gmail.com>
2024-03-07 18:33:35 +0600

    product cart practice

---
+++ cart.js
@@ -0,0 +1,18 @@
+const addProduct = () => {
+  const productField = document.getElementById('product_name');
+  const quantityField = document.getElementById('product_quantity');
+  const product = productField.value;
+  const quantity = quantityField.value;
+  productField.value = '';
+  quantityField.value = '';
+
+  console.log(product, quantity);
+  displayProduct(product, quantity);
+}
+
+const displayProduct = (product, quantity) => {
+  const ul = document.getElementById('product_container');
+  const li = document.createElement('li');
+  li.innerText = `${product} - ${quantity}`;
+  ul.appendChild(li);
+}
